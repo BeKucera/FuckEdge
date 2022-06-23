eeb7230371bafc9322642e4163644f3aa4ef5779
BeKucera <bekucera@gmail.com>
2022-06-23 07:56:44 +0200

    Oprava

--- FuckEdge/Program.cs
+++ FuckEdge/Program.cs
@@ -12,18 +12,10 @@ namespace FuckEdge
 {
     internal class Program
     {
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        const int SW_HIDE = 0;
-        const int SW_SHOW = 5;
-
         static void Main(string[] args)
         {
-            ShowWindow(GetConsoleWindow(),SW_HIDE);
+            Console.WriteLine("Ahoj");
+
             if (args.Length == 0)
             {
                 Process.Start("chrome.exe");
