97de1619620aeed6db1d0a98e536e924d982c122
BeKucera <bekucera@gmail.com>
2022-06-23 07:17:38 +0200

    d

--- FuckEdge/Program.cs
+++ FuckEdge/Program.cs
@@ -3,27 +3,49 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace FuckEdge
 {
     internal class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            ShowWindow(GetConsoleWindow(),SW_HIDE);
+            if (args.Length == 0)
             {
                 Process.Start("chrome.exe");
                 Environment.Exit(0);
             }
+
             string start = "search%3Fq%3D";
             string end = "%26form%3D";
+
+            if (!args[1].Contains(start))
+            {
+                start = "microsoft-edge:https://www.bing.";
+                string adresa = args[1].Substring(args[1].IndexOf(start)+start.Length);
+                Process.Start("https://google."+adresa);
+                
+            }
+
+
             string address = args[1].Substring(args[1].IndexOf(start) + start.Length);
             address = address.Remove(address.IndexOf(end));
-            address = address.Replace("+"," ").Replace("%2B", " ");
             address = $"https://www.google.com/search?q={address}";
-            Process.Start(address );
+            Process.Start(address);
         }
     }
 }
